52bd5284f5fa3181074b3fae882011cbbda32adc
DV <149110822+DV-program@users.noreply.github.com>
2025-05-19 00:48:21 +0300

    Added DependencyInjection

---
+++ backend/Store.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces;
+
+namespace Store.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            services.AddScoped<IStoreDbContext>(provider => 
+                provider.GetService<StoreDbContext>());
+            return services;
+        } 
+    }
+}
